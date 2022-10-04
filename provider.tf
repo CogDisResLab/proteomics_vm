ecd9206307e682fb05b68e9ec1e4dbd8089d5a9a
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2022-10-04 11:50:39 -0400

    feat: add the provider

---
+++ provider.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.33.0"
+    }
+
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+
+  default_tags {
+
+    tags = {
+      Environment = var.project_environment
+      Project     = "Clinical Proteomics"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  alias  = "useast1"
+  default_tags {
+
+    tags = {
+      Environment = var.project_environment
+      Project     = "Clinical Proteomics"
+    }
+  }
+}
